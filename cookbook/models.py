c797eee4ad5225c98ab04df6cd89ed7b42fdf67e
Geoffrey Tang <geoffrey.j.tang@gmail.com>
2018-02-07 15:34:55 -0600

    Created and migrated model: Recipe

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    """A recipe the user wants to record"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
