6322f96959369dbf720420650e638283cf224e9b
Geoffrey Tang <geoffrey.j.tang@gmail.com>
2018-02-08 16:24:18 -0600

    Secured recipes pages to log in and then foreign key relationship to individual users

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -7,7 +7,7 @@ class Recipe(models.Model):
     """A recipe the user wants to record"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    owner =  models.ForeignKey(User)
     def __str__(self):
         """Return a string representation of the model"""
         return self.text
--- mise/settings.py
+++ mise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&5e-rt%765)$2%y+!5vd)(--!&job%g3%ntrdvdg-wqna2l%k-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', "127.0.0.1"]
 
 
 # Application definition
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#My settings
+LOGIN_URL = '/users/login/'
