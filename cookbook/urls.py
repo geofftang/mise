cd30150d9c8ff2a3af24510b884f63e2d5ae184a
geofftang <geoffrey.j.tang@gmail.com>
2018-02-07 21:43:43 -0600

    Added a base parent template and applied it to the home page as well as the newly created recipes page listing out all recipes

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     # Home page
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^recipes/$', views.recipes, name='recipes'),
 ]
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .models import Recipe
+
 # Create your views here.
 
 def index(request):
     """The home page for mise"""
     return render(request, 'cookbook/index.html')
+
+def recipes(request):
+    """Show all recipes"""
+    recipes = Recipe.objects.order_by('date_added')
+    context = {'recipes': recipes}
+    return render(request, 'cookbook/recipes.html', context)
