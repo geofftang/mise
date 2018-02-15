c8943a95ec46feb22bc312e4a00d86c92e98012b
geofftang <geoffrey.j.tang@gmail.com>
2018-02-14 19:02:01 -0600

    Merge issues solved

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -8,13 +8,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Page listing all of a user's recipes
     url(r'^recipes/$', views.recipes, name='recipes'),
-<<<<<<< HEAD
     # Pages for each of a user's recipes
     url(r'^recipes/(?P<recipe_id>\d+)/$', views.recipe, name='recipe'),
     # Page for adding a new recipe
-=======
-    url(r'^recipes/(?P<recipe_id>\d+)/$', views.recipe, name='recipe'),
->>>>>>> 022ce368dc776a2f97ea5acb8c0d43bba97d2c66
     url(r'^new_recipe/$', views.new_recipe, name="new_recipe"),
     # Page for adding a new note
     url(r'^new_note/(?P<recipe_id>\d+)/$', views.new_note, name="new_note"),
--- mise/settings.py
+++ mise/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', "127.0.0.1"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #Third party apps
+    # Third party apps
     'bootstrap3',
-    #My apps
+    # My apps
     'cookbook',
     'users',
 ]
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mise.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -86,25 +83,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -118,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
@@ -151,9 +149,7 @@ if os.getcwd() == '/app':
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
     # Versions of Django before 1.10 require MIDDLEWARE_CLASSES, but the
     # latest version, 1.11.8, had an entirely separate issue with 'flatatt'
